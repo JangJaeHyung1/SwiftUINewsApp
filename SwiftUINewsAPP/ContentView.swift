dee92c050865573091e6006216b4457b3f645430
jh <jh@jhui-MacBookAir.local>
2021-10-26 21:10:43 +0900

    플젝 생성

---
+++ SwiftUINewsAPP/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUINewsAPP
+//
+//  Created by jh on 2021/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+//        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+    }
+}
---
+++ SwiftUINewsAPP/SwiftUINewsAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINewsAPPApp.swift
+//  SwiftUINewsAPP
+//
+//  Created by jh on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINewsAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
